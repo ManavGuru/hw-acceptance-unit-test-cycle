322c01f9ef348497d674ddf5b79230b63817cbfd
EC2 Default User <ec2-user@ip-172-31-2-4.us-east-2.compute.internal>
2021-03-13 04:17:23 +0000

    HW 4 complete

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
-  
+
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
-  
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
     @ratings = params[:ratings]  || session[:ratings] || all_ratings
     @movies = Movie.where( { rating: @ratings.keys } ).order(@sort)
     session[:sort], session[:ratings] = @sort, @ratings
-    
+
     if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
       flash.keep
       redirect_to movies_path sort: @sort, ratings: @ratings
@@ -43,27 +43,32 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
-  def search
-    @similar_movies = Movie.similar_movies(params[:title])
-    if @similar_movies.nil?
-      redirect_to root_url, alert: "'#{params[:title]}' has no director info"
-    end
-    @movie = Movie.find_by(title: params[:title])
-  end
   
+  def similar_movies
+    @movie = Movie.find(params[:title])
+      if @movie.director.nil? || @movie.director.empty?
+        flash[:notice] = "'#{@movie.title}' has no director info"
+        redirect_to root_path
+      else
+        @movies = Movie.similar_movies(params[:title])
+      end
+  end
+
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
+
   private
-    def all_ratings  
-      hash = {}
-      @all_ratings.each { |val| hash[val] = '1' }
-      hash
-    end
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
+  
+  def all_ratings  
+    hash = {}
+    @all_ratings.each { |val| hash[val] = '1' }
+    hash
+  end
+  
 end
--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,10 +1,10 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings; ['G','PG','PG-13','R'] ; 
+    
+    def self.all_ratings; ['G','PG','PG-13','R'] ; 
     end
-  
-  def self.similar_movies movie_title
-    director = Movie.find_by(title: movie_title).director
-    return nil if director.blank? or director.nil?
-    Movie.where(director: director).pluck(:title)
-  end
+    
+    def self.similar_movies(title)
+        director = Movie.find(title).director
+        self.where(director: director)
+    end
 end
